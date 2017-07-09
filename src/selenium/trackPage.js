2ba17b6d30add63fd764f2e7c428e32e7003ce5e
Princu7 <princu7@gmail.com>
2017-07-09 10:23:03 +0530

    Testing easy

--- src/selenium/trackPage.js
+++ src/selenium/trackPage.js
@@ -4,6 +4,10 @@ var until = require('selenium-webdriver').until;
 
 var TrackPage = Object.create(BasePage);
 
+function scrollDown() {
+  window.scrollTo(0, arguments[0]);
+}
+
 TrackPage.getNoOfVisibleSessionElems = function() {
   return this.findAll(By.className('room-filter')).then(this.getElemsDisplayStatus).then(function(displayArr) {
     return displayArr.reduce(function(counter, value) { return value == 1 ? counter + 1 : counter; }, 0);
@@ -13,8 +17,12 @@ TrackPage.getNoOfVisibleSessionElems = function() {
 TrackPage.checkIsolatedBookmark = function() {
   // Sample sessions having ids of 3014 and 3015 being checked for the bookmark feature
   var sessionIdsArr = ['3014', '3015'];
+  var self = this;
 
-  return this.toggleSessionBookmark(sessionIdsArr).then(this.toggleStarredButton.bind(this)).then(this.getNoOfVisibleSessionElems.bind(this));
+  // Bookmark the sessions, scrolls down the page and then count the number of visible session elements
+  return self.toggleSessionBookmark(sessionIdsArr).then(self.toggleStarredButton.bind(self)).then(function() {
+    return self.driver.executeScript(scrollDown, 400).then(self.getNoOfVisibleSessionElems.bind(self));
+  });
 };
 
 module.exports = TrackPage;
